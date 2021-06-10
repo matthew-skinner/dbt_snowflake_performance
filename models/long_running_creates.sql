89beb505cdc7706a761c272ac796e7b3443ce997
matthew-skinner <matthew-skinner@pluralsight.com>
2021-06-10 08:40:51 -0500

    incrementaling long runners

--- models/long_running_creates.sql
+++ models/long_running_creates.sql
@@ -1,5 +1,5 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'incremental'
 ) }}
 
 SELECT
@@ -27,8 +27,7 @@ FROM
 WHERE
     query_load_percent = 100
     AND query_type = 'CREATE_TABLE_AS_SELECT'
+    {# COMMENT THE DATE FILTER OUT TO EMULATE --FULL-REFRESH  #}
     AND TO_DATE(start_time) = DATEADD(DAY, -1, CURRENT_DATE())
     AND queued_overload_time = 0
     AND total_elapsed_time > 300000
-ORDER BY
-    total_elapsed_time DESC
